acb9c39eae73f9066e00c989969eb9bf93ea0972
Patiphan <patiphan.new@mail.kmutt.ac.th>
2021-02-03 10:35:35 +0700

    Hello

--- 62130500049-groupwork-2/main.js
+++ 62130500049-groupwork-2/main.js
@@ -1,15 +1,16 @@
 const app = {
     data() {
         return {
-            message: 'Hello',
-            name: 'Vue3'
+            msg: 'Hello,Vue3',
+            shown: true,
+            url: 'http://www.sit.kmutt.ac.th'
         }
     },
-    created(){​​​​
-        console.log('message is '+this.msg)
-    }​​​​,
-    updated(){
-        console.log('message change is'+this.msg)
-    }
+    // created(){​​​​
+    //     console.log('message is '+this.msg)
+    // }​​​​,
+    // updated(){
+    //     console.log('message change is'+this.msg)
+    // }
 }
 mountedApp = Vue.createApp(app).mount('#app')
